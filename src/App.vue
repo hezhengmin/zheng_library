f916a3274da71e5837908cc7c721268a19f408db
hezhengmin <zhengmin099@gmail.com>
2022-12-25 23:21:10 +0800

    設定 webpack

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav">
+      <router-link to="/">Home</router-link>|<router-link to="/about"
+        >About</router-link
+      >
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+:root {
+  --font: Roboto, sans-serif;
+  --textColor: #374961;
+  --linkActiveColor: #41b783;
 }
-</script>
 
-<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--font);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--textColor);
+
+  .logo {
+    width: 20%;
+  }
+}
+
+.nav {
+  padding: 30px 0 100px 0;
+
+  a {
+    font-weight: 500;
+    color: var(--textColor);
+    margin: 0 5px;
+  }
+
+  a.router-link-exact-active {
+    color: var(--linkActiveColor);
+  }
 }
 </style>
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/main.js",
+  },
+  output: {
+    filename: "[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[name].[contenthash:8].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [{
+            // inject CSS to page
+            loader: 'style-loader'
+        }, {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader'
+        }, {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+                // `postcssOptions` is needed for postcss 8.x;
+                // if you use postcss 7.x skip the key
+                postcssOptions: {
+                    // postcss plugins, can be exported to postcss.config.js
+                    plugins: function () {
+                        return [
+                            require('autoprefixer')
+                        ];
+                    }
+                }
+            }
+        }, {
+            // compiles Sass to CSS
+            loader: 'sass-loader'
+        }]
+    },
+      {
+        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+          outputPath: "assets/img",
+          esModule: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.runtime.esm.js",
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
